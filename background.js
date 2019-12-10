befd969de4e9cd9f46e71b6f19435386ba3199d0
Ronald Wong <ronald.cy.wong@gtomato.com>
2019-12-10 13:14:52 +0800

    feat: updated to click the icon to open in incognito and added right click menu item

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function () {
                         new chrome.declarativeContent.PageStateMatcher({
                             pageUrl: {
                                 schemes: ['https', 'http']
-                            },
+                            }
                         })
                     ],
                     actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -15,4 +15,31 @@ chrome.runtime.onInstalled.addListener(function () {
             ]
         );
     });
+
+    chrome.contextMenus.create({
+        id: "incognitOpenRightClickMenu",
+        title: "Open in Incognito Mode",
+        contexts: ["all"]
+    });
+
+    chrome.contextMenus.onClicked.addListener(function (info, tab) {
+        if (info.menuItemId == "incognitOpenRightClickMenu") {
+            openToIncognito(tab);
+        }
+    });
+
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        openToIncognito(tab);
+    });
+
 });
+
+function openToIncognito(tab) {
+    if (tab.url.startsWith('http')) {
+        chrome.windows.getCurrent({ populate: true }, function (window) {
+            const currentTab = window.tabs.find(tab => tab.active)
+            chrome.windows.create({ url: currentTab.url, incognito: true });
+        })
+    };
+};
+
